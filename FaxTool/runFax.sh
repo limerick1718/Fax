904f68ad13303c32c71ea674da4d6a65e1aff753
JIAKUN LIU <jiakunliu17@outlook.com>
2024-04-13 23:23:39 +0800

    runnable but buggy version

--- FaxTool/runFax.sh
+++ FaxTool/runFax.sh
@@ -1,2 +1,13 @@
-python script/run.py 
-
+jenv local 1.8
+export LD_LIBRARY_PATH="$PATH:/mnt/hdd2/jkliu/Projects/ATGEmpirical/tools/Fax/FaxTool/lib/libz3"
+export ANDROID_HOME="/mnt/hdd2/jkliu/Projects/ATGEmpirical/tools/Fax/ProjectAndroidSdk"
+export PATH="$ANDROID_HOME/tools:$PATH"
+export PATH="$ANDROID_HOME/platform-tools:$PATH"
+export PATH="$ANDROID_HOME/tools/bin:$PATH"
+export PATH="$ANDROID_HOME/build-tools/30.0.0-preview:$PATH"
+export ANT_HOME="/mnt/hdd2/jkliu/Projects/ATGEmpirical/tools/Fax/apache-ant-1.9.16"
+export PATH="$ANT_HOME/bin:$PATH"
+android create project
+ant -version
+which adb
+/usr/bin/python2.7 script/run.py 
---
+++ setup.md
@@ -0,0 +1,20 @@
+# Pre
+
+## Download dependencies
+
+### Download ant 1.9.16
+
+```bash
+wget https://downloads.apache.org/ant/binaries/apache-ant-1.9.16-bin.zip
+unzip apache-ant-1.9.16-bin.zip
+```
+
+### Download Android SDK Tools 25.2.2
+
+```bash
+mkdir ProjectAndroidSdk
+cd ProjectAndroidSdk
+wget http://dl-ssl.google.com/android/repository/tools_r25.2.2-linux.zip
+unzip tools_r25.2.2-linux.zip
+android update sdk --no-ui 
+```
